b370de87dfbc1d4b4b4042329dc8d6385a5183cb
Ramil Mavliutov <stirk2281337@gmail.com>
2023-06-18 03:26:52 +0500

    Create db, create user model. Create simple authorization

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from typing import Union
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi_users import FastAPIUsers
+
+from auth.database import User
+from auth.auth import auth_backend
+from auth.schemas import UserRead, UserCreate
+from auth.manager import get_user_manager
 
 app = FastAPI()
 
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app = FastAPI()
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)) -> str:
+    return f"Hello, {user.email}"
